1bcafa814c471e738aa0069b30d2d7b6beafe945
Micho001 <abdulojotulesuleiman@gmail.com>
2023-03-27 18:12:13 +0100

    updated

--- functions1.c
+++ functions1.c
@@ -128,7 +128,7 @@ int flags, int width, int precision, int size)
  * Return: Number of chars printed
  */
 int print_hexa(va_list types, char map_to[], char buffer[],
-	int flags, char flag_ch, int width, int precision, int size)
+int flags, char flag_ch, int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
